8b4c559d31047ba6a48ff5bf08bb5d88e2258ca8
SM123-byte <mailswagat@gmail.com>
2025-09-14 21:37:58 +0100

    Add files via upload

---
+++ convert.py
@@ -0,0 +1,21 @@
+# Decimal to Binary Conversion 
+
+n = int(input("Enter a number: "))
+result = ""
+
+# Using modulus and division to list binary numbers
+
+while n > 0:
+    n1 = n
+    n = int(n/2)
+    r = n1 % 2
+
+    # Code used to concatenate the strings 
+
+    result = (str(r) + str(result))
+
+print(f"The binary conversion for your number is: {result}")
+    
+    
+    
+
